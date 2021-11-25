98bf894254fc24571ac070d89497fe748a8d5c17
Gerhard Sletten <gerhardsletten@gmail.com>
2021-11-25 09:14:09 +0100

    rename

--- wp-theme/webpack.mix.js
+++ wp-theme/webpack.mix.js
@@ -3,4 +3,12 @@ let mix = require('laravel-mix');
 mix.js('assets/main.js', './js/scripts.js')
     .postCss('assets/style.css', './', [
     require('tailwindcss'),
-  ])
+  ]).browserSync({
+    proxy: 'localhost:8000',
+	   files: [
+      'style.css',
+		  'js/scripts.js',
+		  'assets/**/*',
+		  '**/*.php',
+	  ]
+  })
